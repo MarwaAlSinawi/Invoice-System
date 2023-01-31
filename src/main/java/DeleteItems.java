efd123d763bd055e84b9c39aeb45445f50d5836b
MarwaAlSinawi <116248656+MarwaAlSinawi@users.noreply.github.com>
2023-01-31 09:15:06 +0400

    delete items by id

---
+++ src/main/java/DeleteItems.java
@@ -0,0 +1,33 @@
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class DeleteItems {
+public static void deleteItemsTable() {
+		
+		String url = "jdbc:sqlserver://localhost:1433;databaseName=InvoiceSystem;encrypt=true;trustServerCertificate=true";
+        String user = "sa";
+        String pass = "root";
+        Connection con = null;
+        try {
+            Driver driver = (Driver) Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
+            DriverManager.registerDriver(driver);
+            con = DriverManager.getConnection(url, user, pass);
+            Statement st = con.createStatement();
+            Scanner sa=new Scanner(System.in);
+        	System.out.println("Enter id that you want to delete: ");
+            int idinput =sa.nextInt();
+            int count=0;
+            String sql ="DELETE FROM Items WHERE Items_id= '"+idinput+"'";
+            System.out.println("Item deleted!!");
+            ResultSet rs=st.executeQuery(sql);
+            }
+        catch (Exception ex) {
+            System.err.println(ex);
+        }
+
+}
+}
--- src/main/java/ManageShopItems.java
+++ src/main/java/ManageShopItems.java
@@ -32,7 +32,8 @@ public class ManageShopItems {
 					break;
 
 				case 2:
-
+					DeleteItems.deleteItemsTable();
+					ManageShopItemsSubMenu();
 					break;
 
 				case 3:
